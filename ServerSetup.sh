4e6e1f1bf089654eb0243f050d6d38ea2f3ecd69
IArentBen <benoit.charles@outlook.com>
2021-03-21 10:46:53 -0600

    Inital

---
+++ ServerSetup.sh
@@ -0,0 +1,42 @@
+##############
+# SHOW LOGOS #
+##############
+cd ~
+clear
+curl --silent -o-  | bash
+#######################
+# START SETUP TOOLING #
+#######################
+# Update and Upgrade
+# set up email
+# Auto Update with email notification
+# ssh
+# Snapraid
+# Mergerfs
+# Log Watch
+curl --silent -o-  | bash
+curl --silent -o-  | zsh
+
+source ~/.zshrc
+
+##############
+# NEXT STEPS #
+##############
+# Can I just run this?
+echo "SOME THINGS TO DO..."
+echo "*******************************************************"
+echo "* ssh-keygen -t ed25519 -C \"your_email@example.com\" *"
+echo "*******************************************************"
+echo "*************************************************************"
+echo "* cd ~ && git clone https://github.com/bcbrian/.scripts.git *"
+echo "*************************************************************"
+echo "**********************************"
+echo "* open nvim and run plug install *"
+echo "**********************************"
+echo "***************************************"
+echo "* setup nvim config file to load mine *"
+echo "***************************************"
+
+
+
+
