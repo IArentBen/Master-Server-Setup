62a8eb0e82cae4a71709d95dedac96dc1f7f7776
Ben <IArentBen@users.noreply.github.com>
2021-03-25 15:31:02 -0600

    Started other main installs

--- ServerSetup.sh
+++ ServerSetup.sh
@@ -19,45 +19,45 @@ sudo apt update -y && sudo apt upgrade -y
 # config email
 #check to see if postfix is installed
 if $(command -v postfix >/dev/null)
-    then 
-        echo "Postfix is already installed"
-        echo "Do you wish to config this program?"
-        while true; do
-            read -p "Do you wish to config this program?" yn
-            case $yn in
-                [Yy]* ) curl --silent -o- https://raw.githubusercontent.com/IArentBen/Master-Server-Setup/main/Postfix.sh | bash; break;;
-                [Nn]* ) exit;;
-                * ) echo "Please answer yes or no.";;
-            esac
-        done
-    else echo "Installing Postfix"
+then
+echo "Postfix is already installed"
+echo "Do you wish to config this program?"
+while true; do
+read -p "Do you wish to config this program?" yn
+case $yn in
+[Yy]*) curl --silent -o- https://raw.githubusercontent.com/IArentBen/Master-Server-Setup/main/Postfix.sh | bash; break;;
+[Nn]*) exit;;
+*) echo "Please answer yes or no.";;
+esac
+done
+else echo "Installing Postfix"
 sudo apt-get install libsasl2-modules mailutils postfix -y
 #call postfix config script
 curl --silent -o- https://raw.githubusercontent.com/IArentBen/Master-Server-Setup/main/Postfix.sh | bash
 echo "Postfix is now installed and setup"
-fi 
+fi
 
 # install and config git
 
 #check to see if Git is installed
 if $(command -v git >/dev/null)
-    then 
-        echo "git is already installed"
-        echo "Do you wish to config git?"
-        while true; do
-            read -p "Do you wish to config git?" yn
-            case $yn in
-                [Yy]* ) curl --silent -o- https://raw.githubusercontent.com/IArentBen/Master-Server-Setup/main/git.sh | bash; break;;
-                [Nn]* ) exit;;
-                * ) echo "Please answer yes or no.";;
-            esac
-        done
-    else echo "Installing git"
+then
+echo "git is already installed"
+echo "Do you wish to config git?"
+while true; do
+read -p "Do you wish to config git?" yn
+case $yn in
+[Yy]*) curl --silent -o- https://raw.githubusercontent.com/IArentBen/Master-Server-Setup/main/git.sh | bash; break;;
+[Nn]*) exit;;
+*) echo "Please answer yes or no.";;
+esac
+done
+else echo "Installing git"
 sudo apt install git -y
 #Call Git config script
 curl --silent -o- https://raw.githubusercontent.com/IArentBen/Master-Server-Setup/main/git.sh | bash
 echo "git is now installed and setup"
-fi 
+fi
 
 
 # Auto Update with email notification
@@ -66,16 +66,72 @@ fi
 
 # ssh
 
-# Snapraid
-
 # Mergerfs
-
+if $(command -v mergerfs >/dev/null)
+then
+echo "mergerfs is already installed"
+while true; do
+read -p "Do you wish to config mergerfs?" yn
+case $yn in
+[Yy]*) curl --silent -o- https://raw.githubusercontent.com/IArentBen/Master-Server-Setup/main/mergerfs.sh | bash; break;;
+[Nn]*) exit;;
+*) echo "Please answer yes or no.";;
+esac
+done
+else echo "Installing mergerfs"
+#Call mergerfs config script
 sudo wget https://github.com/trapexit/mergerfs/releases/download/2.32.4/mergerfs_2.32.4.debian-buster_amd64.deb -P /home/$USER/bin/
 sudo dpkg -i mergerfs_2.32.4.debian-buster_amd64.deb
+echo "mergerfs is now installed and setup"
+fi
+#Snapraid
+if $(command -v snapraid >/dev/null)
+then
+echo "snapraid is already installed"
+while true; do
+read -p "Do you wish to config snapraid?" yn
+case $yn in
+[Yy]*) curl --silent -o- https://raw.githubusercontent.com/IArentBen/Master-Server-Setup/main/snapraid.sh | bash; break;;
+[Nn]*) exit;;
+*) echo "Please answer yes or no.";;
+esac
+done
+else echo "Installing snapraid"
+#Call snapraid config script
+echo "snapraid is now installed and setup"
+fi
 # Log Watch
-
+if $(command -v Logwatch >/dev/null)
+    then 
+        echo "Logwatch is already installed"
+        while true; do
+            read -p "Do you wish to config Logwatch?" yn
+            case $yn in
+                [Yy]* ) curl --silent -o- https://raw.githubusercontent.com/IArentBen/Master-Server-Setup/main/Logwatch.sh | bash; break;;
+                [Nn]* ) exit;;
+                * ) echo "Please answer yes or no.";;
+            esac
+        done
+    else echo "Installing Logwatch"
+#Call Logwatch config script
+echo "Logwatch is now installed and setup"
+fi 
 # Docker and Docker-Compose
-
+if $(command -v Docker and Docker Compose >/dev/null)
+    then 
+        echo "Docker and Docker Compose is already installed"
+        while true; do
+            read -p "Do you wish to config Docker and Docker Compose?" yn
+            case $yn in
+                [Yy]* ) curl --silent -o- https://raw.githubusercontent.com/IArentBen/Master-Server-Setup/main/Docker and Docker Compose.sh | bash; break;;
+                [Nn]* ) exit;;
+                * ) echo "Please answer yes or no.";;
+            esac
+        done
+    else echo "Installing Docker and Docker Compose"
+#Call Docker and Docker Compose config script
+echo "Docker and Docker Compose is now installed and setup"
+fi 
 #curl --silent -o-  | bash
 #curl --silent -o-  | zsh
 #removes variables from shell
