8be0e8677896d9c8e9afab73905ff75aaddda6f0
docker <docker@docker.charles.place>
2021-03-25 21:08:36 -0600

    added docker.sh script

---
+++ docker.sh
@@ -0,0 +1,7 @@
+sudo apt install apt-transport-https ca-certificates 
+curl gnupg2 software-properties-common 
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key 
+add - 
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable" 
+sudo apt updatesudo apt install docker-ce 
+sudo curl -L https://github.com/docker/compose/releases/download/1.28.6/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose sudo chmod +x /usr/local/bin/docker-compose
