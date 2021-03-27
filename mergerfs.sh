f16accdd6f7fa171fd38d045314b8aef97621ff7
Ben <IArentBen@users.noreply.github.com>
2021-03-27 07:10:39 -0600

    update mergerfs.sh

--- mergerfs.sh
+++ mergerfs.sh
@@ -1,2 +1,11 @@
 echo 'test mergerfs'
 
+#copying info FSTAB
+
+UUID=$UUIDDISK0   /mnt/disk0 ext4 defaults 0 0
+UUID=$UUIDDISK1   /mnt/disk1 ext4 defaults 0 0
+UUID=$UUIDDISK2   /mnt/disk2 ext4 defaults 0 0
+UUID=$UUIDPARITY  /mnt/parity ext4 defaults 0 0
+ 
+# mergerfs pool
+/mnt/disk* /srv/storage fuse.mergerfs direct_io,defaults,allow_other,minfreespace=50G,fsname=mergerfs 0 0
